9d25c35716d9a44b15cfdbf89891aa9f29f1e6cb
Lucas Santos <43353219+iFallenHunt@users.noreply.github.com>
2022-06-29 21:30:50 -0300

    Add files via upload

---
+++ aprovado/nota_aluno.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// a nota deve ser digitada com numeros inteiros
+
+int main(){
+	
+	int n1, n2, nfim;
+	
+	printf("Digite a primeira nota: ", n1);
+	scanf("%d", &n1);
+	
+	printf("Digite a segunda nota: ", n1);
+	scanf("%d", &n2);
+	
+	nfim = n1+n2;
+	
+	if (nfim >= 60){
+		printf("Nota %d, \nAluno aprovado", nfim);
+	}else{
+		printf("Nota %d, \nAluno reprovado", nfim);
+	}
+}
